13eb59eb1b3393093e5d38ca03e2f82eca65d9b1
Manoj k <manatharv@gmail.com>
2023-01-10 10:17:25 +0530

    changes made by ashok

---
+++ src/main/java/com/cg/Test.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Hey Maven you are so wonderful tool ");
+                   // he will use all the spring features and hibernate features
+   // start writing your spring project
+   
+	}
+
+}
---
+++ src/test/java/com/cg/MyTest.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
